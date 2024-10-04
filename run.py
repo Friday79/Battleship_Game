40ea89917fea1a091b8bc6df2938245137672d7c
Friday79 <fuwuigbe@gmail.com>
2024-10-04 18:42:12 +0000

    self.grid_size, self.grid.,self.ships,self.num_ships and self.place_ships written

--- run.py
+++ run.py
@@ -6,4 +6,8 @@ Class BattleshipGame:
     The Game is initialize by given the grid_size and 
     the computer ships.
     """
-    
+  self.grid_size = grid_size
+  self.grid = [["0"]]*grid_size for _ in range(grid_size)
+  self.ship = []
+  self.num_ships = grid_size // 2
+  self.place_ships()  
