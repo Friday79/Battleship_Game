d792d4017d5bcc245282d8b34266e65a44b0e44e
Friday79 <fuwuigbe@gmail.com>
2024-10-04 19:07:06 +0000

    the function to place ship in the grid written

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
 import random
 
-Class BattleshipGame:
- def_init_(self,grid_size):
+class BattleshipGame:
+ def _init_(self, grid_size):
     """
     The Game is initialize by given the grid_size and 
     the computer ships.
     """
-  self.grid_size = grid_size
+self.grid_size = grid_size
   self.grid = [["0"]]*grid_size for _ in range(grid_size)
   self.ship = []
   self.num_ships = grid_size // 2
   self.place_ships()  
+
+  def place_ships(self):
+    """
+    randomly place ship on the grid
+    """   
